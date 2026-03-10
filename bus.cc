0952d98a2e92352a23577ae0a8e55caca1cf55b5
Harshit23Bisht <harshitbisht.7258@gmail.com>
2026-03-10 23:21:00 +0530

    Update bus.cc

--- bus.cc
+++ bus.cc
@@ -1,26 +1,52 @@
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/csma-module.h"
 #include "ns3/netanim-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
 
 using namespace ns3;
 
-int main() {
-    NodeContainer nodes;
-    nodes.Create(18);
+int main ()
+{
+NodeContainer nodes;
+nodes.Create(20);
 
-    CsmaHelper csma;
-    csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
-    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+InternetStackHelper stack;
+stack.Install(nodes);
 
-    csma.Install(nodes);
+PointToPointHelper p2p;
+p2p.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+p2p.SetChannelAttribute("Delay", StringValue("2ms"));
 
-    InternetStackHelper stack;
-    stack.Install(nodes);
+for(int i=0;i<19;i++)
+{
+NodeContainer pair(nodes.Get(i), nodes.Get(i+1));
+p2p.Install(pair);
+}
 
-    AnimationInterface anim("bus.xml");
 
-    Simulator::Run();
-    Simulator::Destroy();
+MobilityHelper mobility;
+
+mobility.SetPositionAllocator(
+"ns3::GridPositionAllocator",
+"MinX", DoubleValue(0.0),
+"MinY", DoubleValue(0.0),
+"DeltaX", DoubleValue(10.0),
+"DeltaY", DoubleValue(0.0),
+"GridWidth", UintegerValue(20),
+"LayoutType", StringValue("RowFirst"));
+
+mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+mobility.Install(nodes);
+
+/* -------------------------------------- */
+
+AnimationInterface anim("bus.xml");
+
+Simulator::Stop(Seconds(10));
+Simulator::Run();
+Simulator::Destroy();
+
+return 0;
 }
