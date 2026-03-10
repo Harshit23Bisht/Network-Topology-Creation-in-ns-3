7118b6057a9f873f16d224c3733e30ed07c332c9
Harshit23Bisht <harshitbisht.7258@gmail.com>
2026-03-10 23:22:39 +0530

    Update tree.cc

--- tree.cc
+++ tree.cc
@@ -1,40 +1,84 @@
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
 #include "ns3/netanim-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/internet-module.h"
 
 using namespace ns3;
 
-int main() {
-    NodeContainer nodes;
-    nodes.Create(20);
+int main ()
+{
+NodeContainer root;
+root.Create(1);
 
-    PointToPointHelper p2p;
-    p2p.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-    p2p.SetChannelAttribute("Delay", StringValue("2ms"));
+NodeContainer level1;
+level1.Create(4);
 
-    InternetStackHelper stack;
-    stack.Install(nodes);
+NodeContainer level2;
+level2.Create(16);
 
-    AnimationInterface anim("tree.xml");
+NodeContainer allNodes;
+allNodes.Add(root);
+allNodes.Add(level1);
+allNodes.Add(level2);
 
-    int parent = 0;
-    int child = 1;
+InternetStackHelper stack;
+stack.Install(allNodes);
 
-    while (child < 20) {
-        NodeContainer pair(nodes.Get(parent), nodes.Get(child));
-        p2p.Install(pair);
+PointToPointHelper p2p;
+p2p.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+p2p.SetChannelAttribute("Delay", StringValue("2ms"));
 
-        child++;
-        if (child < 20) {
-            NodeContainer pair2(nodes.Get(parent), nodes.Get(child));
-            p2p.Install(pair2);
-            child++;
-        }
-        parent++;
-    }
+/* root -> level1 */
 
-    Simulator::Run();
-    Simulator::Destroy();
+for(int i=0;i<4;i++)
+{
+NodeContainer pair(root.Get(0), level1.Get(i));
+p2p.Install(pair);
+}
+
+/* level1 -> level2 */
+
+int index = 0;
+
+for(int i=0;i<4;i++)
+{
+for(int j=0;j<4;j++)
+{
+NodeContainer pair(level1.Get(i), level2.Get(index));
+p2p.Install(pair);
+index++;
+}
+}
+
+AnimationInterface anim("tree.xml");
+
+/* ---- Set clean tree positions ---- */
+
+anim.SetConstantPosition(root.Get(0),50,10);
+
+/* level 1 */
+
+anim.SetConstantPosition(level1.Get(0),20,30);
+anim.SetConstantPosition(level1.Get(1),40,30);
+anim.SetConstantPosition(level1.Get(2),60,30);
+anim.SetConstantPosition(level1.Get(3),80,30);
+
+/* level 2 */
+
+int posX = 10;
+
+for(int i=0;i<16;i++)
+{
+anim.SetConstantPosition(level2.Get(i),posX,60);
+posX +=5;
+}
+
+/* ------------------------------ */
+
+Simulator::Stop(Seconds(10));
+Simulator::Run();
+Simulator::Destroy();
+
+return 0;
 }
